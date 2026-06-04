513136a4b4b1a52c484a7a782d7c9d1e4cf50738
Johana Terrier <johana.terrier@apollossc.com>
2026-06-04 15:42:59 +0200

    Fix : protection des url #107

--- app/middleware/admin-restriction.global.ts
+++ app/middleware/admin-restriction.global.ts
@@ -1,9 +1,17 @@
-import { Capacitor } from '@capacitor/core'
+import { Capacitor } from "@capacitor/core";
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (to.path.startsWith('/admin')) {
-    if (process.client && Capacitor.isNativePlatform()) {
-      return navigateTo('/')
+  const authToken = useCookie("auth_token");
+  const userRole = useCookie("user_role");
+  const isNative = process.client && Capacitor.isNativePlatform();
+
+  if (to.path.startsWith("/admin")) {
+    if (isNative) {
+      return navigateTo("/");
+    }
+
+    if (!authToken.value || String(userRole.value) !== "2") {
+      return navigateTo("/");
     }
   }
-})
+});
